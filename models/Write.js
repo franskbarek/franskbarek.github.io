b76f38d7396b367d52cd906d45fb2e6033452fde
frans <franzferdinand.kbarek@gmail.com>
2023-02-12 12:17:53 +0900

    adding spec writes schema

--- models/Write.js
+++ models/Write.js
@@ -7,6 +7,19 @@ const WriteSchema = new mongoose.Schema(
       required: true,
       unique: true,
     },
+    releaseDate: {
+      type: String,
+      required: true,
+    },
+    readTime: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+      default: "franskbarek",
+    },
     photo: {
       type: String,
       required: false,
