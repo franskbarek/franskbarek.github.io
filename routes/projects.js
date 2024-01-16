ba32512f0e1a062d96e86426fcdceec14bffff26
franskbarek <franzferdinand.kbarek@gmail.com>
2024-01-16 21:10:07 +0900

    adding function in projects routing

--- routes/projects.js
+++ routes/projects.js
@@ -3,52 +3,54 @@ const router = express.Router();
 import Project from "../models/Project.js";
 
 // create
-router.post("/", async (req, res) => {
-  const newProject = new Project(req.body);
-  try {
-    const savedProject = await newProject.save();
-    res.status(200).json(savedProject);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+// router.post("/", async (req, res) => {
+//   const newProject = new Project(req.body);
+//   try {
+//     const savedProject = await newProject.save();
+//     res.status(200).json(savedProject);
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 
 // update
-router.put("/:id", async (req, res) => {
-  try {
-    const project = Project.finById(req.params.id);
-    if (project.username.id === req.body.username) {
-      try {
-        const updatedProject = await Project.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true });
-        res.status(200).json(updatedProject);
-      } catch (err) {
-        res.status(500).json(err);
-      }
-    } else {
-      res.status(401).json("You can update only your project!");
-    }
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+// router.put("/:id", async (req, res) => {
+//   try {
+//     const project = Project.finById(req.params.id);
+//     if (project.username.id === req.body.username) {
+//       try {
+//         const updatedProject = await Project.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true });
+//         res.status(200).json(updatedProject);
+//       } catch (err) {
+//         res.status(500).json(err);
+//       }
+//     } else {
+//       res.status(401).json("You can update only your project!");
+//     }
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
 // delete
-router.delete("/:id", async (req, res) => {
-  try {
-    const project = await Project.findById(req.params.id);
-    if (project.username.id === req.body.username) {
-      try {
-        project.delete();
-        res.status(200).json("Project has been deleted!");
-      } catch (err) {
-        res.status(500).json(err);
-      }
-    } else {
-      res.status(401).json("Your can delete only your project!");
-    }
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+// router.delete("/:id", async (req, res) => {
+//   try {
+//     const project = await Project.findById(req.params.id);
+//     if (project.username.id === req.body.username) {
+//       try {
+//         project.delete();
+//         res.status(200).json("Project has been deleted!");
+//       } catch (err) {
+//         res.status(500).json(err);
+//       }
+//     } else {
+//       res.status(401).json("Your can delete only your project!");
+//     }
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
 // get
 router.get("/:id", async (req, res) => {
   try {
@@ -68,5 +70,40 @@ router.get("/", async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+router.get("/pagination", async (req, res) => {
+
+  // pagination
+  // using category for search, exp: http://localhost:5000/backend/projects?page=1&limit=4
+  const page = parseInt(req.query.page) || 1; //by default 1
+  const limit = parseInt(req.query.limit) || 4 //required and flexible
+
+  try {
+    if(page > 0 && limit > 0){
+      const result = await Project.find().skip((page - 1) * limit).limit(limit).sort({ createdAt: -1 });
+      res.status(200).json(result);
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+router.get("/search", async (req, res) => {
+
+   // filter using regex
+   const qSearch = req.query.search;
+
+  try {
+    const regex = new RegExp(`\\b(${qSearch.split(" ").join("|")})\\b`, "gi");
+    const result = await Project.find({ title: regex });
+      res.status(200).json(result);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+
 
 export default router;
